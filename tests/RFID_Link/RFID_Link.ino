bd378753ac7eadc3638e9a5c3fac7c69ca459ee7
david-beaudette <david.beaudette@usherbrooke.ca>
2015-02-24 23:06:33 -0500

    Managed to send back received number from RPi.

--- tests/RFID_Link/RFID_Link.ino
+++ tests/RFID_Link/RFID_Link.ino
@@ -24,14 +24,19 @@ const int rfResetPin  = 9;      // RC522 reset signal
 const int rfSdaPin    = 10;     // RC522 SDA signal
 
 // Radio configuration
-RF24 radio(radioCePin, radioCsnPin);
-const uint64_t pipes[2]    = { 0xF0F0F0F0E1LL, 0xF0F0F0F0D2LL };
-const uint8_t radioChannel = 10;
+const uint64_t pipes[2]    = {0xF0F0F0F0E1LL, 0xF0F0F0F0D2LL};
+const uint8_t radioChannel = 0x4c;
+const uint8_t payloadSize  = 8;
+const int retryDelay       = 15;
+const int retryCount       = 15;
+const rf24_datarate_e dataRate = RF24_1MBPS;
 
 // Other program constants
 const int quickFlash = 500;    // duration in ms for quickly flashing a LED
+const int slowFlash  = 1000;   // duration in ms for slowly flashing a LED
 
 // Declare radio
+RF24 radio(radioCePin, radioCsnPin);
 
 void setup() {
   // Reset digital outputs
@@ -46,43 +51,47 @@ void setup() {
   SPI.begin();
   Serial.begin(57600);
   printf_begin();
-  printf("\n\rRFID_Link/Test/\n\r");
-
-  Serial.println("Looking for NRF24L01+.");
-  radio.begin();
-  Serial.println("Radio began.");
-  radio.setRetries(15,15);
-  Serial.println("Retry config set.");
-  radio.setPayloadSize(8);
-  Serial.println("Payload size set.");
-  radio.openWritingPipe(pipes[1]);
-  Serial.println("Opened writing pipe.");
-  radio.openReadingPipe(1,pipes[0]);
-  Serial.println("Opened reading pipe.");
   
-  radio.startListening();
-  Serial.println("Started listening.");
-  radio.setChannel(0x4c);
+  Serial.println("RFID_Link Test");
+  Serial.println("Configuring NRF24L01+.");
+  
+  radio.begin();
+ // Configure as slave in the communication protocol
+  radio.setRetries(retryDelay, retryCount);
+  radio.setPayloadSize(payloadSize);
+  radio.openWritingPipe(pipes[1]);
+  radio.openReadingPipe(1,pipes[0]);
+  radio.setChannel(radioChannel);
   radio.enableDynamicPayloads();
   radio.setAutoAck(true);
-  radio.setDataRate(RF24_1MBPS);
+  radio.setDataRate(dataRate);
+  
+  radio.startListening();
   radio.printDetails();
+  
+  Serial.println("Started listening.");
+  
+  // Signal self-test success
+  flashLed(grnLedPin, quickFlash);
+  delay(quickFlash);
+  flashLed(grnLedPin, quickFlash);
+  delay(quickFlash);
+  flashLed(grnLedPin, quickFlash);
 }
 
 void loop() {
   
-  //Serial.println("Looping.");
   // Check for received packet
   if (radio.available())
   {
     // Dump the payloads until we've gotten everything
-    unsigned long rx_buf;
+    uint8_t rx_buf;
     bool done = false;
     while (!done)
     {
       // Fetch the payload, and see if this was the last one.
-      done = radio.read(&rx_buf, sizeof(unsigned long));
-      printf("Got payload %lu...", rx_buf);
+      done = radio.read(&rx_buf, 1);
+      //printf("Received %d\r\n", rx_buf);
   
       // Delay just a little bit to let the other unit
       // make the transition to receiver
@@ -90,16 +99,23 @@ void loop() {
     }
   
     // First, stop listening so we can talk
-    //radio.stopListening();
+    radio.stopListening();
   
-    // Send the final one back.
-    //radio.write(&rx_buf, sizeof(unsigned long));
-    //printf("Sent response.\n\r");
+    // Send the received number.
+    if(radio.write(&rx_buf, 1)) {
+      printf("Received and sent back %d.\n\r", rx_buf);
+    }
   
     // Now, resume listening so we catch the next packets.
-    //radio.startListening();
+    radio.startListening();
   }
   // Wait before checking for another packet
-  //delay(1000);
+  flashLed(redLedPin, slowFlash);
+  delay(slowFlash);
 }
 
+void flashLed(const int pinNum, const int duration_ms) {
+  digitalWrite(pinNum, HIGH);
+  delay(duration_ms);
+  digitalWrite(pinNum, LOW);
+}
