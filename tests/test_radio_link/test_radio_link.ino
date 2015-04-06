2a6b5cba1bdd5da1352b2b307fe4228366207fb5
david-beaudette <david.beaudette@usherbrooke.ca>
2015-04-06 16:13:55 -0400

    Updated test_radio_link with HAL header.

--- tests/test_radio_link/test_radio_link.ino
+++ tests/test_radio_link/test_radio_link.ino
@@ -14,69 +14,28 @@
 #include "RF24.h"
 #include "printf.h"
 
-// GPIO pin numbers
-const int redLedPin   = 3;      // Red LED
-const int grnLedPin   = 4;      // Green LED
-const int radioCsnPin = 5;      // NRF24L01+ CSN signal
-const int radioCePin  = 6;      // NRF24L01+ CE signal
-const int relayPin    = 7;      // Relay
-const int rfResetPin  = 9;      // RC522 reset signal
-const int rfSdaPin    = 10;     // RC522 SDA signal
-
-// Radio configuration
-const uint64_t pipes[2]    = {0xF0F0F0F0E1LL, 0xF0F0F0F0D2LL};
-const uint8_t radioChannel = 0x4c;
-const uint8_t payloadSize  = 10;
-const int retryDelay       = 15;
-const int retryCount       = 15;
-const rf24_datarate_e dataRate = RF24_1MBPS;
-
-// Other program constants
-const int quickFlash = 500;    // duration in ms for quickly flashing a LED
-const int slowFlash  = 10;   // duration in ms for slowly flashing a LED
+#include "nano_rfid_hal.h"
 
 // Declare radio
 RF24 radio(radioCePin, radioCsnPin);
 
 void setup() {
   // Reset digital outputs
-  pinMode(relayPin,  OUTPUT);
-  pinMode(redLedPin, OUTPUT);
-  pinMode(grnLedPin, OUTPUT);
-  
-  digitalWrite(relayPin,  LOW);
-  digitalWrite(redLedPin, LOW);
-  digitalWrite(grnLedPin, LOW);
+  SetPins();
 
   SPI.begin();
-  Serial.begin(57600);
+  Serial.begin(serialRate);
   printf_begin();
   
   Serial.println("RFID_Link Test");
   Serial.println("Configuring NRF24L01+.");
   
-  radio.begin();
- // Configure as slave in the communication protocol
-  radio.setRetries(retryDelay, retryCount);
-  radio.setPayloadSize(payloadSize);
-  radio.openWritingPipe(pipes[1]);
-  radio.openReadingPipe(1,pipes[0]);
-  radio.setChannel(radioChannel);
-  radio.enableDynamicPayloads();
-  radio.setAutoAck(true);
-  radio.setDataRate(dataRate);
-  
-  radio.startListening();
-  radio.printDetails();
+  RadioConfig(&radio);
   
   Serial.println("Started listening.");
   
   // Signal self-test success
-  flashLed(grnLedPin, quickFlash);
-  delay(quickFlash);
-  flashLed(grnLedPin, quickFlash);
-  delay(quickFlash);
-  flashLed(grnLedPin, quickFlash);
+  FlashLed(grnLedPin, quickFlash, 3);
 }
 
 void loop() {
@@ -182,12 +141,7 @@ void loop() {
     radio.startListening();
   }
   // Wait before checking for another packet
-  flashLed(redLedPin, slowFlash);
+  FlashLed(grnLedPin, slowFlash, 1);
   delay(slowFlash);
 }
 
-void flashLed(const int pinNum, const int duration_ms) {
-  digitalWrite(pinNum, HIGH);
-  delay(duration_ms);
-  digitalWrite(pinNum, LOW);
-}
