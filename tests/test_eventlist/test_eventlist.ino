cd029d99d66f5dddf836f4b19a523a00adeaf18f
david-beaudette <david.beaudette@usherbrooke.ca>
2015-04-06 15:23:26 -0400

    Updated test_eventlist with HAL header.

--- tests/test_eventlist/test_eventlist.ino
+++ tests/test_eventlist/test_eventlist.ino
@@ -12,20 +12,12 @@
 
 #include <Event.h>
 #include <Timer.h>
-
 #include <MFRC522.h>
+
+#include "nano_rfid_hal.h"
 #include "AccessEvent.h"
 
-void FlashLed(const int pinNum, const int duration_ms, int num_times = 1);
-void Stall();
 void DumpLogging();
-
-// GPIO pin numbers
-const int redLedPin   = 3;      // Red LED
-const int grnLedPin   = 4;      // Green LED
-const int relayPin    = 7;      // Relay
-const int rfResetPin  = 9;      // RC522 reset signal
-const int rfSdaPin    = 10;     // RC522 SDA signal
 
 // Declare card reader
 MFRC522 nfc(rfSdaPin, rfResetPin);
@@ -42,9 +34,6 @@ EventList eventList(&t, 50);
 // Card tag read flag
 int read_tag_flag = 1;
 
-const int quickFlash = 500;    // duration in ms for quickly flashing a LED
-const int slowFlash  = 1000;   // duration in ms for slowly flashing a LED
-
 // Event type will cycle through the possible 
 // Type is one of the following:
 //  0x30 : ‘Attempt’ (first authorization in double authorization mode).
@@ -56,16 +45,10 @@ int event_type = 0x30;
 
 void setup() {
   // Reset digital outputs
-  pinMode(relayPin,  OUTPUT);
-  pinMode(redLedPin, OUTPUT);
-  pinMode(grnLedPin, OUTPUT);
-  
-  digitalWrite(relayPin,  LOW);
-  digitalWrite(redLedPin, LOW);
-  digitalWrite(grnLedPin, LOW);
+  SetPins();
 
   SPI.begin();
-  Serial.begin(115200);
+  Serial.begin(serialRate);
   
   Serial.println("EventList and RFID test");
   Serial.println("---------");
@@ -119,24 +102,6 @@ void loop() {
       t.after(1000, SetReadFlag);
     }
   }   
-}
-
-void FlashLed(const int pinNum, const int duration_ms, int num_times) {
-  for(int i = 0; i < num_times; i++) {
-    digitalWrite(pinNum, HIGH);
-    delay(duration_ms);
-    digitalWrite(pinNum, LOW);
-    if(i < (num_times-1)) {
-      delay(duration_ms);
-    }
-  }
-}
-
-void Stall() {
-  while(1) {
-    FlashLed(redLedPin, quickFlash, 2);
-    delay(2000);
-  }
 }
 
 void DumpLogging() {
